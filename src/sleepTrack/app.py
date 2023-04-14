305d933ee338ee345a258093fafe733a0afcc961
Mary Nesterenko <mn@banksiaglobal.com>
2023-04-15 01:18:16 +0400

    fix cors 1

--- src/sleepTrack/app.py
+++ src/sleepTrack/app.py
@@ -3,22 +3,24 @@ from fastapi.middleware.cors import CORSMiddleware
 from . import api
 from fastapi.middleware.httpsredirect import HTTPSRedirectMiddleware
 
-app = FastAPI()
 
+
+
+
+
+app = FastAPI(
+    title='SleepTracker',
+    version='1.0.0',
+)
 
 origins = ["*"]
-# app.add_middleware(HTTPSRedirectMiddleware)
+app.add_middleware(HTTPSRedirectMiddleware)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
-)
-
-app = FastAPI(
-    title='SleepTracker',
-    version='1.0.0',
 )
 
 app.include_router(api.router)
